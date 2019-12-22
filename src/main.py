feccc93a9214867974410877b086e582cdd7b029
rolledm <rolledm@gmail.com>
2019-12-22 04:07:18 +0300

    Get cars of user. Get parts of car.

---
+++ src/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient()
+table = client.cph.cph
+driver = 'rolledm'
+car = 'rolledm_car'
+
+@app.route('/cph/api/v1.0/parts/<string:nickname>', methods=['GET'])
+def get_cars(nickname):
+    obj = table.find_one({'nickname': nickname})['cars']
+    cars = []
+    for iter in obj:
+        cars.append(f"{iter['name']} \'{iter['codename']}\''")
+    return jsonify(cars)
+
+@app.route('/cph/api/v1.0/parts/<string:nickname>/<string:car>', methods=['GET'])
+def get_parts(nickname, car):
+    obj = table.find_one({'nickname': nickname})['cars']
+    car_info = 0
+    for iter in obj:
+        if iter['codename'] == car:
+            car_info = iter
+            break
+    return jsonify(car_info)
+
+if __name__ == '__main__':
+    app.run(debug=True)
