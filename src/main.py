f6108858e97982416b7071ff00378830ac43183b
rolledm <rolledm@gmail.com>
2019-12-22 16:02:31 +0300

    v1.0. Get, add, remove for each category.

--- src/main.py
+++ src/main.py
@@ -5,23 +5,29 @@ app = Flask(__name__)
 
 client = MongoClient()
 table = client.cph.cph
-driver = 'rolledm'
-car = 'rolledm_car'
 
-@app.route('/cph/api/v1.0/parts', methods=['GET'])
 def get_users():
     obj = table.find({})
     users = []
     for iter in obj:
         users.append(f"{iter['name']} \'{iter['nickname']}\'")
-    return jsonify(users)
+    return users
+
+@app.route('/cph/api/v1.0/parts', methods=['GET'])
+def get_available_users():
+    return jsonify(get_users())
 
 @app.route('/cph/api/v1.0/parts/createUser', methods=['POST'])
 def create_user():
     name = request.form.get('name')
     nickname = request.form.get('nickname')
     table.insert_one({'name': name, 'nickname': nickname, 'cars': []})
-    return get_users()
+    return get_available_users()
+
+@app.route('/cph/api/v1.0/parts/removeUser/<string:nickname>', methods=['POST'])
+def remove_user(nickname):
+    table.remove({'nickname': nickname})
+    return get_available_users()
 
 def get_cars(nickname):
     obj = table.find_one({'nickname': nickname})['cars']
@@ -56,7 +62,6 @@ def remove_car(nickname, codename):
     table.save(person)
     return get_available_cars(nickname)
 
-@app.route('/cph/api/v1.0/parts/<string:nickname>/<string:car>', methods=['GET'])
 def get_parts(nickname, car):
     obj = table.find_one({'nickname': nickname})['cars']
     car_info = 0
@@ -64,7 +69,41 @@ def get_parts(nickname, car):
         if iter['codename'] == car:
             car_info = iter
             break
-    return jsonify(car_info)
+    return car_info
+
+@app.route('/cph/api/v1.0/parts/<string:nickname>/<string:car>', methods=['GET'])
+def get_available_parts(nickname, car):
+    return jsonify(get_parts(nickname, car))
+
+@app.route('/cph/api/v1.0/parts/<string:nickname>/<string:car>/createPart', methods=['POST'])
+def create_part(nickname, car):
+    name = request.form.get('name')
+    mileage1 = request.form.get('mileage1')
+    mileage2 = request.form.get('mileage2')
+    obj = table.find_one({'nickname': nickname})
+    for iter in obj['cars']:
+        if iter['codename'] == car:
+            iter['parts'].append({'name': name, 'mileage1': mileage1, 'mileage2': mileage2})
+            break
+    table.save(obj)
+    return get_parts(nickname, car)
+
+@app.route('/cph/api/v1.0/parts/<string:nickname>/<string:car>/removePart/<string:name>', methods=['POST'])
+def remove_part(nickname, car, name):
+    person = table.find_one({'nickname': nickname})
+    pointer = 0
+    for iter in person['cars']:
+        if iter['codename'] == car:
+            for iter1 in iter['parts']:
+                if iter1['name'] == name:
+                    pointer = iter1
+                    break
+            iter['parts'].remove(pointer)
+            break
+            
+    table.save(person)
+    return get_available_parts(nickname, car)
+
 
 
 if __name__ == '__main__':
