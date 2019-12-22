0787f0d913ca75aa7fe6207507228993a287e7f6
rolledm <rolledm@gmail.com>
2019-12-22 04:42:27 +0300

    Get list of users.

--- src/main.py
+++ src/main.py
@@ -8,12 +8,20 @@ table = client.cph.cph
 driver = 'rolledm'
 car = 'rolledm_car'
 
+@app.route('/cph/api/v1.0/parts/', methods=['GET'])
+def get_users():
+    obj = table.find({})
+    users = []
+    for iter in obj:
+        users.append(f"{iter['name']} \'{iter['nickname']}\'")
+    return jsonify(users)
+
 @app.route('/cph/api/v1.0/parts/<string:nickname>', methods=['GET'])
 def get_cars(nickname):
     obj = table.find_one({'nickname': nickname})['cars']
     cars = []
     for iter in obj:
-        cars.append(f"{iter['name']} \'{iter['codename']}\''")
+        cars.append(f"{iter['name']} \'{iter['codename']}\'")
     return jsonify(cars)
 
 @app.route('/cph/api/v1.0/parts/<string:nickname>/<string:car>', methods=['GET'])
@@ -25,6 +33,7 @@ def get_parts(nickname, car):
             car_info = iter
             break
     return jsonify(car_info)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
